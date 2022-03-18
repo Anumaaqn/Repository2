e7a6bafa30822b066dc04cb99b71dff260b17f0e
User <User@us>
2022-03-18 13:34:26 +0800

    ll

---
+++ cs-311/src/Lab2/Lab2App.java
@@ -0,0 +1,43 @@
+package Lab2;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Lab2App {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Lab2App window = new Lab2App();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Lab2App() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
